8da536671c2c821764e6cf9affb9d2be99754084
fangyunhe <fangyunhe@mingbikes.com>
2018-01-18 18:28:55 +0800

    添加zuul

---
+++ cloud-service-gateway-zuul/src/main/java/org/cloud/service/gateway/zuul/GatewayZuulLaunch.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.cloud.service.gateway.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Description:服务网关,所有的请求均到达这个服务进行过滤、认证、授权
+ * 访问服务的端口为网关的端口
+ * @author:fangyunhe
+ * @time:2018年1月18日 下午4:41:56
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayZuulLaunch {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayZuulLaunch.class, args);
+	}
+}
